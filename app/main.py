fdc7de6e61c783bf6472e256491823e175fafde7
nedonadeem93 <nedonadeem93@gmail.com>
2026-09-11 20:26:54 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from __future__ import annotations
-
 import logging
 from contextlib import asynccontextmanager
 from pathlib import Path
